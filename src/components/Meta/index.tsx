40fd6b69a72b989d7409226ff668c660418150ad
Ptreeraganont <ptreeraganont@myseneca.ca>
2022-10-19 23:18:08 +0700

    change description

--- src/components/Meta/index.tsx
+++ src/components/Meta/index.tsx
@@ -13,7 +13,7 @@ export default function Meta({ title: titleString }: MetaProps) {
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
       />
-      <meta name="description" content="iCath is a website that provide information about medical" />
+      <meta name="description" content="i-Cath an online educational tool for cardiac catheterization learning" />
       <meta property="og:image" content="/images/ogimage.jpg" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
