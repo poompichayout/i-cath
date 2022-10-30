d5614c40d6bdac9632883205bceedbd2e5179711
Ptreeraganont <ptreeraganont@myseneca.ca>
2022-10-30 14:52:42 +0700

    fix: ogimage and backdrop color

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
+
+import Grid from '@mui/material/Grid'
 
 import Meta from 'src/components/Meta'
 
@@ -6,6 +9,16 @@ const Home: NextPage = () => {
   return (
     <>
       <Meta title="Home" />
+      <Grid container sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Grid item>
+          <Image
+            src="/images/ogimage.png"
+            width={719.5}
+            height={500}
+            alt="icath"
+          />
+        </Grid>
+      </Grid>
     </>
   )
 }
--- src/theme/overrides/index.ts
+++ src/theme/overrides/index.ts
@@ -6,7 +6,6 @@ import Paper from './Paper'
 import Input from './Input'
 import Button from './Button'
 import Tooltip from './Tooltip'
-import Backdrop from './Backdrop'
 import Typography from './Typography'
 import CssBaseline from './CssBaseline'
 import Autocomplete from './Autocomplete'
@@ -19,7 +18,6 @@ export default function ComponentsOverrides(theme: Theme) {
     Paper(theme),
     Button(theme),
     Tooltip(theme),
-    Backdrop(theme),
     Typography(theme),
     CssBaseline(theme),
     Autocomplete(theme)
